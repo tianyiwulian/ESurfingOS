78024af08b5798d281ffab399caa31685db2a001
zhanghm <zhanghm@sttri.com.cn>
2019-09-18 11:50:58 +0800

    Commit the baseline version 1.0.0 of ESuringOS

---
+++ demos/ctiot_mqtt/ctiot_device_info.h
@@ -0,0 +1,15 @@
+#ifndef _CTIOT_DEVICE_INFO_H
+#define _CTIOT_DEVICE_INFO_H
+
+#define DEVICE_NO "0001"
+#define DEVICE_ID "100065690001"
+#define DEVICE_PROTOCOL "MQTT"
+#define DEVICE_AUTHORIZATION "token"
+#define DEVICE_TOKEN "TMsf33Vg0P28kFki93igA1OiGz0LGJvt3g1s29Ow8m0"
+#define DEVICE_TRANSPARENT 0
+#define DEVICE_TCPADDRESS "www.ctwing.cn"
+#define DEVICE_TCPPORT "1883"
+#define DEVICE_TLSADDRESS "www.ctwing.cn"
+#define DEVICE_TLSPORT "8883"
+
+#endif //_CTIOT_DEVICE_INFO_H
